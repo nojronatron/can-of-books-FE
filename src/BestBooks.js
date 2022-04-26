bb7bb868d342e1c5fd16bd0e8f53003b2aec8d3c
zarate <zarate.rauljr@gmail.com>
2022-04-25 17:31:50 -0700

    added some code front end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-boostrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
+import img from './img/slidetemp.JPG';
+import Image from 'react-bootstrap/Image';
 require('dotenv').config();
 
 class BestBooks extends React.Component {
@@ -39,25 +41,24 @@ async componentDidMount() {
     /* TODO: render all the books in a Carousel */
 
     return (
+
+
       <Carousel>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+              {this.state.books.map((element, idx) =>
+              (
 
-        {this.state.books.length ? (
-           this.state.books.forEach((item, idx) => {
-
-            <Carousel.Item id={idx}>
-              <Carousel.Caption>
-                <h3>{item.title}</h3>
-                <h4>{item.description}</h4>
-                <p>{item.status}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          })
-          )
-        : (
-          <h3>No Books Found :(</h3>
-        )}
-
+              <Carousel.Item key={idx}>
+                <Image
+                className='d-block w-100'
+                src={img}
+                alt='a slide'
+                />
+                  <h3>{element.title}</h3>
+                  <h4>{element.description}</h4>
+                  <p>{element.status}</p>
+              </Carousel.Item>
+              ))}
       </Carousel>
         
     )
