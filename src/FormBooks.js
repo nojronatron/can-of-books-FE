85db1b53301c527a45062573ad2b84b48d773400
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-28 11:24:32 -0700

    merge garbage in file removed, started testing.

--- src/FormBooks.js
+++ src/FormBooks.js
@@ -1,58 +1,5 @@
-<<<<<<< HEAD
-import { Component } from 'react';
-import { Modal, Form, Button } from 'react-bootstrap';
-
-class FormBooks extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newBook: {},
-      title: '',
-      description: ''
-    }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let book = {
-      title: e.target.title.value,
-      description: e.target.description.value,
-      status: e.target.status.value,
-      etext: e.target.eText.value,
-      
-     
-    }
-    this.props.addBook(book);
-  }
-
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId='title'>
-          <Form.Label>title</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
-        <Form.Group controlId='description'>
-          <Form.Label>description</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
-        <Form.Group controlId='status'>
-          <Form.Label>status</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
-        <Form.Group controlId='eText'>
-          <Form.Check type="checkbox" label='eText' />
-        </Form.Group>
-        <Button type='submit'>Add a Book</Button>
-      </Form>
-    );
-  }
-}
-
-=======
 import { Component } from 'react';
 import { Form, Button, Modal } from 'react-bootstrap';
-
 class FormBooks extends Component {
   constructor(props) {
     super(props);
@@ -107,5 +54,4 @@ class FormBooks extends Component {
   }
 }
 
->>>>>>> dev-lab13
 export default FormBooks;
